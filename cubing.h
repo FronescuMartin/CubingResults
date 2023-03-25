1dbbc66ac3beb2be380cf2d0b0eb75f3c3c5aa54
FronescuMartin <116015854+FronescuMartin@users.noreply.github.com>
2023-03-25 18:37:06 +0200

    Add files via upload

--- cubing.h
+++ cubing.h
@@ -2,14 +2,22 @@
 #define CUBING_H_INCLUDED
 #include <iostream>
 using namespace std;
+enum Events{
+    _2x2, _3x3, _4x4, _5x5, _6x6, _7x7, Megaminx, Pyraminx, OneHanded, Blindfolded, Square_1, MultiBLD, _4BLD, _5BLD, Skewb, FewestMovesChallenge, Clock,
+};
 
+enum SingleRecordTypes{
+    PR_Single, NR_Single, CR_Single, WR_Single, No_Single_Record
+};
+enum AverageRecordTypes{
+    PR_Average, NR_Average, CR_Average, WR_Average, No_Average_Record
+};
 class String{
     char *str;
 public:
     String();
     String(char *str);
     String(const String &s);
-    //void print();
     ~String();
     String operator +(const String &s2);
     String& operator =(const String &s);
@@ -19,44 +27,99 @@ public:
 
 };
 
+enum Month{
+    January=1, February=2, March=3, April=4, May=5, June=6, July=7, August=8, September=9, October=10, November=11, December=12
+};
+
+class Date{
+private:
+    int day;
+    int month;
+    int year;
+public:
+    Date(int _day, Month _month, int _year);
+    Date(int _day, int _month, int _year);
+    friend ostream& operator << (ostream &out, Date &d);
+};
+class Competition;
+
+class Result{
+private:
+
+    Events event;
+    double times[5];
+    int times_len;
+    double single, average;
+
+    SingleRecordTypes singleRecord;
+    AverageRecordTypes averageRecord;
+    Competition* competition;
+    int competitionId;
+    int rankInRound;
+public:
+    Result();
+    Result(Events event, double _times[], int length, int _rank, SingleRecordTypes recordSg=No_Single_Record, AverageRecordTypes recorAvg=No_Average_Record, int _comp_id=-1);
+    void print();
+    void calculateAverageAndSingle();
+    double getAverage();
+    double getSingle();
+    double roundTo2DecimalPlaces(double num);
+    Result operator=(const Result& r){
+        event=r.event;
+        //times=r.times;
+        times_len=r.times_len;
+        for(int i=0; i<times_len; i++){
+            times[i]=r.times[i];
+        }
+        single=r.single;
+        average=r.average;
+        singleRecord=r.singleRecord;
+        averageRecord=r.averageRecord;
+        competition=r.competition;
+        rankInRound=r.rankInRound;
+        return *this;
+    }
+    //getCompetitionPointerFromId;
+};
+
 class Competitor{
 private:
     String name, country;
     int id, numberOfNRs, numberOfCRs, numberOfWRs, age;
-    //Result results[];
+    int results_len;
+    Result* results;
+    int indexCrt=0;
+
     //Competition competitions[];
     //Result bestResults;
 public:
     Competitor(String name, String country, int id, int age);
+    ~Competitor();
+    void addResultData(Events event, double _times[], int length, int _rank, SingleRecordTypes recordSg=No_Single_Record, AverageRecordTypes recorAvg=No_Average_Record, int _comp_id=-1);
+    void addResults(Result _results);
     String getName();
     void setAge(int newAge);
     int numberOfRecords();
     void print();
 
 };
-class Result{
+
+
+
+class Competition{
 private:
-    enum Events{
-        _2x2, _3x3, _4x4, _5x5, _6x6, _7x7, Megaminx, Pyraminx, OneHanded, Blindfolded, Square_1, MultiBLD, _4BLD, _5BLD, Skewb, FewestMovesChallenge, Clock,
-    };
-    Events event;
-    double *times;
-    int times_len;
-    double single, average;
-    enum RecordTypes{
-        NR_Single, NR_Average, CR_Single, CR_Average, WR_Single, WR_Average, No_Record
-    };
-    RecordTypes record;
-    //Competition* competition;
-    int rankInRound;
+    String name;
+    //Date date;
+    int numberOfCompetititors;
+    int id;
 public:
-    Result(/*Events event,*/double times[], int length, int _rank/*, RecordTypes record*/);
-    ~Result();
-    void calculateAverageAndSingle();
-    double getAverage();
-    double getSingle();
-    double roundTo2DecimalPlaces(double num);
+    Competition(String _name, int _numberOfCompetitors, int _id);
+
 
 };
+
+
+
+
 
 #endif // CUBING_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ String::String(const String &s){
 }
 
 String& String::operator =(const String &s){
+    delete[] str;
     str=new char[strlen(s.str)+1];
     strcpy(str, s.str);
     str[strlen(s.str)]='\0';
@@ -42,6 +43,7 @@ String String::operator + (const String &s2){
     strncpy(tmp_str+strlen(str), s2.str, strlen(s2.str));
     strcpy(tmp_str+(strlen(str)+strlen(s2.str)),"\0");
     String tmp(tmp_str);
+    delete[] tmp_str;
     //cout<<tmp_str<<'\n';
     return tmp;
 }
@@ -55,6 +57,7 @@ String String::operator + (char ch){
     strcpy(tmp_str+strlen(str), chv);
     strcpy(tmp_str+(strlen(str)+1),"\0");
     String tmp(tmp_str);
+    delete[] tmp_str;
     //cout<<tmp_str<<'\n';
     return tmp;
 }
@@ -64,10 +67,23 @@ ostream& operator <<(ostream &out, String &s){
 }
 //##########COMPETITOR###########
 Competitor::Competitor(String name, String country, int id, int age){
+    //results=new Result*[results_len];
+    this->results=nullptr;
     this->name=name;
     this->country=country;
     this->id=id;
     this->age=age;
+    this->results_len=0;
+    this->numberOfNRs=0;
+    this->numberOfCRs=0;
+    this->numberOfWRs=0;
+
+}
+
+Competitor::~Competitor(){
+    if(results!=nullptr){
+        delete[] results;
+    }
 }
 String Competitor::getName(){
     return this->name;
@@ -76,31 +92,124 @@ void Competitor::setAge(int newAge){
     this->age=newAge;
 }
 int Competitor::numberOfRecords(){
+    cout<<numberOfNRs<<' '<<numberOfCRs<<' '<<numberOfWRs<<'\n';
     return numberOfNRs+numberOfCRs+numberOfWRs;
 }
 void Competitor::print(){
     cout<<"Nume: "<<this->name<<'\n';
     cout<<"Tara: "<<this->country<<'\n';
     cout<<"Numar Recorduri: "<<this->numberOfRecords()<<'\n';
+    for(int i=0; i<results_len; i++){
+        results[i].print();
+    }
+}
+void Competitor::addResultData(Events event, double _times[], int length, int _rank, SingleRecordTypes recordSg, AverageRecordTypes recordAvg, int _comp_id){
+    Result _result(event, _times, length, _rank, recordSg, recordAvg, _comp_id);
+    Result* result2=new Result[results_len+1];
+    for(int i=0; i<results_len; i++){
+        result2[i]=results[i];
+    }
+    result2[results_len]=_result;
+    delete[] results;
+    results=result2;
+    results_len++;
+    if(recordSg==NR_Single){
+        numberOfNRs++;
+    }
+    if(recordAvg==NR_Average){
+        numberOfNRs++;
+    }
+    if(recordSg==CR_Single){
+        numberOfCRs++;
+    }
+    if(recordAvg==CR_Average){
+        numberOfCRs++;
+    }
+    if(recordSg==WR_Single){
+        numberOfWRs++;
+    }
+    if(recordAvg==WR_Average){
+        numberOfWRs++;
+    }
+
+}
+
+void Competitor::addResults(Result _result){
+    Result* result2=new Result[results_len+1];
+    for(int i=0; i<results_len; i++){
+        result2[i]=results[i];
+    }
+    result2[results_len]=_result;
+    delete[] results;
+    results=result2;
+    results_len++;
+
+    //results[this->indexCrt]=_results;
+    //this->indexCrt++;
+    //cout<<indexCrt;
+}
+//###########DATE############
+Date::Date(int _day, Month _month, int _year){
+    this->day=_day;
+    this->month=_month;
+    this->year=_year;
+}
+
+
+
+Date::Date(int _day, int _month, int _year){
+    this->day=_day;
+    this->month=_month;
+    this->year=_year;
+}
+
+ostream& operator << (ostream &out, Date &d){
+    if(d.day<10){
+        out<<'0';
+    }
+    out<<d.day<<'.';
+    if(d.month<10){
+        out<<'0';
+    }
+    out<<d.month<<'.';
+    out<<d.year;
+    return out;
+
+}
+//###########COMPETITION#########
+Competition::Competition(String _name, int _numberOfCompetitors, int _id){
+    this->name=_name;
+    this->numberOfCompetititors=_numberOfCompetitors;
+    this->id=_id;
 }
 
 //###########RESULT############
-Result::Result(/*Events event, */double _times[], int length, int _rank/*, RecordTypes record*/){
-    //this->event=event;
+Result::Result(Events event, double _times[], int length, int _rank, SingleRecordTypes recordSg, AverageRecordTypes recorAvg, int _comp_id){
+    this->event=event;
     this->times_len=length;
-    this->times=new double[times_len];
     this->rankInRound=_rank;
+    this->singleRecord=recordSg;
+    this->averageRecord=recorAvg;
+    this->competitionId=_comp_id;
     for(int i=0; i<times_len; i++){
         times[i]=_times[i];
-        cout<<_times[i]<<'\n';
+        //cout<<_times[i]<<'\n';
     }
     //cout<<this->times[2];
     calculateAverageAndSingle();
     //this->record=record;
 }
-
+Result::Result(){
+    times_len=0;
+}
+void Result::print(){
+    for(int i=0; i<times_len; i++){
+        cout<<times[i]<<' ';
+    }
+    cout<<'\n';
+}
 double Result::roundTo2DecimalPlaces(double num){
-    return round(num * 100) / 100;
+    return round(num * 100) / 100.0;
 }
 
 void Result::calculateAverageAndSingle(){
@@ -127,15 +236,40 @@ double Result::getSingle(){
     return this->single;
 }
 
-Result::~Result(){
-    delete[] times;
-}
 
 int main()
 {
     double temp[5]={1.23,1.24,1.09,1.93,2.21};
-    Result r(temp, 5, 2);
-    cout<<r.getAverage();
+    Result r(_2x2,temp, 5, 2);
+    //cout<<r.getAverage()<<'\n';
+    Date d(23, February, 2013);
+    //cout<<d;
+    //String s1("Feliks Zemdegs");
+    //String s2("Australia");
+    Competitor c1(String("Feliks Zemdegs"), String("Australia"), 1, 23);
+    double tempTimes[5]={1.23,1.24, 1.25, 2.43, 4.53};
+    double tempTimes2[5]={2.23,3.24, 1.25, 7.43, 4.53};
+    double tempTimes3[5]={6.23,5.24, 1.25, 2.43, 4.53};
+    double tempTimes4[5]={1.23,2.24, 1.25, 1.03, 0.93};
+
+    //sa se aloce inauntru!!!
+    c1.addResultData(_2x2, tempTimes,5,4,No_Single_Record, WR_Average, 4);
+    c1.addResultData(_2x2, tempTimes2,5,4,No_Single_Record, WR_Average, 4);
+
+    c1.print();
+    //Result temp2(_2x2, tempTimes,5,4,No_Single_Record, No_Average_Record, 4);
+    //Result temp3(_2x2, tempTimes,5,4,No_Single_Record, No_Average_Record, 4);
+    //Result temp4(_3x3, tempTimes,5,4,NR_Single, NR_Average, 4);
+
+    //Result* tempArr=new Result*{&temp1, &temp2, &temp3};
+    //c1.addResults(temp1);
+    //c1.addResults(temp2);
+    //c1.addResults(temp3);
+    //c1.addResults(temp4);
+
+    //c1.addResults(&temp3);
+
+
 
     return 0;
 }
